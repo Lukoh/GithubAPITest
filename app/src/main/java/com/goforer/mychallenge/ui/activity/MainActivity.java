8e6a8d1d6a81d57df3ea47c2e68617fb07a6fa8c
Lukoh <lukoh.nam@gmail.com>
2016-12-08 21:06:02 +0900

    Update MainActivity.java

--- app/src/main/java/com/goforer/mychallenge/ui/activity/MainActivity.java
+++ app/src/main/java/com/goforer/mychallenge/ui/activity/MainActivity.java
@@ -134,7 +134,7 @@ public class MainActivity extends AppCompatActivity {
 
             MainActivity activity = activityWeakRef.get();
             if (activity != null) {
-                for(int i = 1; i < items.size(); i++) {
+                for(int i = 0; i < items.size(); i++) {
                     mAdapter.addItem((Repos)items.get(i));
                 }
 
